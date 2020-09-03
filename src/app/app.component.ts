8d0c3c3e85602b2c94b60c67229a34c2cb9f50e2
peopletech-coder <sekhar.net123@gmail.com>
2020-09-03 07:48:15 +0530

    Adding a new Repository

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) {}
+ 
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+  Hi(a){
+    alert(a)
+    
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent
+  ],
+  imports: [
+    BrowserModule,ModalModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
